40bf28ff1e4891a053e8c3d5195592f73ce4be0a
sid <673088860@qq.com>
2020-03-21 00:34:11 +0800

    FIX : 用户端无法收到proxychannel中抛出的异常信息

--- src/main/java/com/github/monkeywie/proxyee/exception/HttpProxyExceptionHandle.java
+++ src/main/java/com/github/monkeywie/proxyee/exception/HttpProxyExceptionHandle.java
@@ -1,6 +1,14 @@
 package com.github.monkeywie.proxyee.exception;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class HttpProxyExceptionHandle {
 
@@ -10,6 +18,31 @@ public class HttpProxyExceptionHandle {
 
     public void afterCatch(Channel clientChannel, Channel proxyChannel, Throwable cause)
             throws Exception {
-        throw new Exception(cause);
+
+        ByteArrayOutputStream baos = null;
+
+        try {
+
+            baos = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(baos);
+            cause.printStackTrace(printStream);
+
+            ByteBuf byteBuf = clientChannel.alloc().heapBuffer(baos.size());
+            byteBuf.writeBytes(baos.toByteArray());
+
+            HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.BAD_GATEWAY,byteBuf);
+            clientChannel.writeAndFlush(response);
+            if (clientChannel.pipeline().get("httpCodec") != null) {
+                clientChannel.pipeline().remove("httpCodec");
+            }
+
+        } finally {
+
+            if (baos != null) {
+                baos.close();
+            }
+
+        }
     }
 }
