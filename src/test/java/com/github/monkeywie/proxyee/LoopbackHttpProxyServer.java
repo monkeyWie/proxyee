a71c95cd856df07df34a852cadbef19059c6d0ab
liwei <liwei-8466@qq.com>
2021-01-17 13:28:25 +0800

    test: fix loopback test case

--- src/test/java/com/github/monkeywie/proxyee/LoopbackHttpProxyServer.java
+++ src/test/java/com/github/monkeywie/proxyee/LoopbackHttpProxyServer.java
@@ -1,8 +1,19 @@
 package com.github.monkeywie.proxyee;
 
-import com.github.monkeywie.proxyee.resolver.LoopbackAddressResolverGroup;
 import com.github.monkeywie.proxyee.server.HttpProxyServer;
 import com.github.monkeywie.proxyee.server.HttpProxyServerConfig;
+import io.netty.resolver.AddressResolver;
+import io.netty.resolver.AddressResolverGroup;
+import io.netty.resolver.DefaultNameResolver;
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.Promise;
+import io.netty.util.internal.SocketUtils;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 
 public class LoopbackHttpProxyServer {
 
@@ -16,6 +27,43 @@ public class LoopbackHttpProxyServer {
         new HttpProxyServer()
                 .serverConfig(config)
                 .start(9999);
+    }
+
+    private static class LoopbackAddressResolverGroup extends AddressResolverGroup<InetSocketAddress> {
+        public static final LoopbackAddressResolverGroup INSTANCE = new LoopbackAddressResolverGroup();
+
+        private LoopbackAddressResolverGroup() {
+        }
+
+        @Override
+        protected AddressResolver<InetSocketAddress> newResolver(EventExecutor executor) {
+            return (new LoopbackNameResolver(executor)).asAddressResolver();
+        }
+
+        private static class LoopbackNameResolver extends DefaultNameResolver {
+
+            public LoopbackNameResolver(EventExecutor executor) {
+                super(executor);
+            }
+
+            @Override
+            protected void doResolve(String inetHost, Promise<InetAddress> promise) {
+                try {
+                    promise.setSuccess(SocketUtils.addressByName("localhost"));
+                } catch (UnknownHostException unknownHostException) {
+                    promise.setFailure(unknownHostException);
+                }
+            }
+
+            @Override
+            protected void doResolveAll(String inetHost, Promise<List<InetAddress>> promise) {
+                try {
+                    promise.setSuccess(Arrays.asList(SocketUtils.allAddressesByName("localhost")));
+                } catch (UnknownHostException unknownHostException) {
+                    promise.setFailure(unknownHostException);
+                }
+            }
+        }
     }
 
 
