c516af93a25cea966e2a7af565b58478548c5fc8
Eric Peters <eric@peters.org>
2022-02-10 09:08:30 -0800

    * Add HttpProxyAuthenticationProvider.authenticate(HttpRequest request) * Add HttpProxyAuthenticationProvider.matches(HttpRequest request) * Update test/AuthHttpProxyServer.java to include test

--- src/test/java/com/github/monkeywie/proxyee/AuthHttpProxyServer.java
+++ src/test/java/com/github/monkeywie/proxyee/AuthHttpProxyServer.java
@@ -8,11 +8,16 @@ import com.github.monkeywie.proxyee.server.HttpProxyServerConfig;
 import com.github.monkeywie.proxyee.server.auth.BasicHttpProxyAuthenticationProvider;
 import com.github.monkeywie.proxyee.server.auth.HttpAuthContext;
 import com.github.monkeywie.proxyee.server.auth.model.BasicHttpToken;
+import com.github.monkeywie.proxyee.util.ProtoUtil.RequestProto;
 import io.netty.channel.Channel;
+import io.netty.handler.codec.http.*;
+
+import java.nio.charset.StandardCharsets;
 
 public class AuthHttpProxyServer {
 
     // curl -i -x 127.0.0.1:9999 -U admin:123456 https://www.baidu.com
+    // curl -v http://127.0.0.1:9999/status/health
     public static void main(String[] args) throws Exception {
         HttpProxyServerConfig config = new HttpProxyServerConfig();
         config.setAuthenticationProvider(new BasicHttpProxyAuthenticationProvider() {
@@ -23,12 +28,54 @@ public class AuthHttpProxyServer {
                 }
                 return null;
             }
+
+            @Override
+            public boolean matches(HttpRequest request) {
+                if (request.uri().matches("^/status/health$")) {
+                    return false;
+                }
+                return true;
+            }
         });
         new HttpProxyServer()
                 .serverConfig(config)
                 .proxyInterceptInitializer(new HttpProxyInterceptInitializer() {
                     @Override
                     public void init(HttpProxyInterceptPipeline pipeline) {
+                        pipeline.addLast(new HttpProxyIntercept() {
+                            private boolean isDirect = false;
+
+                            @Override
+                            public void beforeRequest(Channel clientChannel, HttpRequest httpRequest, HttpProxyInterceptPipeline pipeline) throws Exception {
+                                RequestProto requestProto = pipeline.getRequestProto();
+                                if (!requestProto.getProxy() && httpRequest.uri().matches("^/status/health$")) {
+                                    isDirect = true;
+
+                                    HttpResponse httpResponse = new DefaultHttpResponse(HttpVersion.HTTP_1_1,
+                                            HttpResponseStatus.OK);
+
+                                    String res = "OK";
+                                    byte[] bts = res.getBytes(StandardCharsets.UTF_8);
+
+                                    httpResponse.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain;charset=utf-8");
+                                    httpResponse.headers().set(HttpHeaderNames.CONTENT_LENGTH, bts.length);
+                                    httpResponse.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
+                                    HttpContent httpContent = new DefaultLastHttpContent();
+                                    httpContent.content().writeBytes(bts);
+                                    clientChannel.writeAndFlush(httpResponse);
+                                    clientChannel.writeAndFlush(httpContent);
+                                    clientChannel.close();
+                                }
+
+                            }
+
+                            @Override
+                            public void beforeRequest(Channel clientChannel, HttpContent httpContent, HttpProxyInterceptPipeline pipeline) throws Exception {
+                                if (!isDirect) pipeline.beforeRequest(clientChannel, httpContent);
+                            }
+                        });
+
+
                         pipeline.addLast(new HttpProxyIntercept() {
                             @Override
                             public void beforeConnect(Channel clientChannel, HttpProxyInterceptPipeline pipeline) throws Exception {
