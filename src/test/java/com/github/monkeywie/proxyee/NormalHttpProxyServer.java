5e20316162729168330158910b392f053da6de2f
monkeyWie <liwei-8466@qq.com>
2022-02-16 16:37:24 +0800

    feat: connect mode handle http (#203)
    * feat: auto handle http packet on connect proxy mode
    
    * build: upgrade to v1.6.1

--- src/test/java/com/github/monkeywie/proxyee/NormalHttpProxyServer.java
+++ src/test/java/com/github/monkeywie/proxyee/NormalHttpProxyServer.java
@@ -1,7 +1,15 @@
 package com.github.monkeywie.proxyee;
 
+import com.github.monkeywie.proxyee.intercept.HttpProxyInterceptInitializer;
+import com.github.monkeywie.proxyee.intercept.HttpProxyInterceptPipeline;
+import com.github.monkeywie.proxyee.intercept.common.FullRequestIntercept;
 import com.github.monkeywie.proxyee.server.HttpProxyServer;
 import com.github.monkeywie.proxyee.server.HttpProxyServerConfig;
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpRequest;
+
+import java.nio.charset.Charset;
 
 public class NormalHttpProxyServer {
 
@@ -14,6 +22,27 @@ public class NormalHttpProxyServer {
         config.setProxyGroupThreads(1);
         new HttpProxyServer()
                 .serverConfig(config)
+                .proxyInterceptInitializer(new HttpProxyInterceptInitializer() {
+                    @Override
+                    public void init(HttpProxyInterceptPipeline pipeline) {
+                        pipeline.addLast(new FullRequestIntercept() {
+
+                            @Override
+                            public boolean match(HttpRequest httpRequest, HttpProxyInterceptPipeline pipeline) {
+                                return true;
+                            }
+
+                            @Override
+                            public void handleRequest(FullHttpRequest httpRequest, HttpProxyInterceptPipeline pipeline) {
+                                ByteBuf content = httpRequest.content();
+                                //打印请求信息
+                                System.out.println(httpRequest.toString());
+                                System.out.println(content.toString(Charset.defaultCharset()));
+                            }
+
+                        });
+                    }
+                })
                 .start(9999);
     }
 }
