21a71416346d0b7f6cab6a00409a02438e4b6ce0
levi <liwei-8466@qq.com>
2022-09-08 11:59:45 +0800

    feat: support forward HttpClientCodec config

--- src/main/java/com/github/monkeywie/proxyee/handler/HttpProxyInitializer.java
+++ src/main/java/com/github/monkeywie/proxyee/handler/HttpProxyInitializer.java
@@ -1,5 +1,6 @@
 package com.github.monkeywie.proxyee.handler;
 
+import com.github.monkeywie.proxyee.server.HttpProxyServerConfig;
 import com.github.monkeywie.proxyee.util.ProtoUtil.RequestProto;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
@@ -27,13 +28,14 @@ public class HttpProxyInitializer extends ChannelInitializer {
         if (proxyHandler != null) {
             ch.pipeline().addLast(proxyHandler);
         }
+        HttpProxyServerConfig serverConfig = ((HttpProxyServerHandler) clientChannel.pipeline().get("serverHandle")).getServerConfig();
         if (requestProto.getSsl()) {
-            ch.pipeline().addLast(
-                    ((HttpProxyServerHandler) clientChannel.pipeline().get("serverHandle")).getServerConfig()
-                            .getClientSslCtx()
-                            .newHandler(ch.alloc(), requestProto.getHost(), requestProto.getPort()));
+            ch.pipeline().addLast(serverConfig.getClientSslCtx().newHandler(ch.alloc(), requestProto.getHost(), requestProto.getPort()));
         }
-        ch.pipeline().addLast("httpCodec", new HttpClientCodec());
+        ch.pipeline().addLast("httpCodec", new HttpClientCodec(
+                serverConfig.getMaxInitialLineLength(),
+                serverConfig.getMaxHeaderSize(),
+                serverConfig.getMaxChunkSize()));
         ch.pipeline().addLast("proxyClientHandle", new HttpProxyClientHandler(clientChannel));
     }
 }
